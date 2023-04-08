bb4eb435ce8cb84cee5722b89d586ac7db332ca1
Taoism DeepLake <pymess@126.com>
2023-04-08 20:50:36 +0800

    tabs

--- src/main/java/com/example/examplemod/RegisterItem.java
+++ src/main/java/com/example/examplemod/RegisterItem.java
@@ -1,16 +1,42 @@
 package com.example.examplemod;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class RegisterItem {
     @SubscribeEvent
-    public static void onItemRegister(RegistryEvent.Register<Item> event)
+    public static void handleItem(RegistryEvent.Register<Item> event)
     {
-        event.getRegistry().register(new Item().setRegistryName("test"));
-    }
+        //vanilla tab
+        event.getRegistry().register(new Item().setRegistryName("test3").setCreativeTab(CreativeTabs.MISC));
 
+        //custom tab
+        event.getRegistry().register(new Item().setRegistryName("test4").setCreativeTab(WRONG));
+    }
+    public static final CreativeTabs CORRECT = new CreativeTabs(CreativeTabs.getNextID(), "newTab2")
+    {
+        @SideOnly(Side.CLIENT)
+        public ItemStack getTabIconItem()
+        {
+            return new ItemStack(Items.NETHER_STAR);
+        }
+    };
+
+    public static final CreativeTabs WRONG = new CreativeTabs(0, "newTab")
+    {
+        @SideOnly(Side.CLIENT)
+        public ItemStack getTabIconItem()
+        {
+            return new ItemStack(Items.NETHER_STAR);
+        }
+    };
 }
