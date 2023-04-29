037ad6938ee5f132dfc55c5f40645cd682952177
Taoism DeepLake <pymess@126.com>
2023-04-29 14:05:44 +0800

    Code for item render

--- src/main/java/com/example/examplemod/RegisterItem.java
+++ src/main/java/com/example/examplemod/RegisterItem.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -7,6 +8,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,14 +27,19 @@ public class RegisterItem {
     public static final ItemBowBase BOW_3 = new ItemBowBase("bow3");
     public static final ItemBowBase BOW_4 = new ItemBowBase("bow4");
 
-
-
-
     @SubscribeEvent
     public static void handleItem(RegistryEvent.Register<Item> event)
     {
         event.getRegistry().registerAll(ITEM_LIST.toArray(new Item[0]));
     }
 
-
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public static void onModelReg(ModelRegistryEvent event)
+    {
+        for (Item item:
+             ITEM_LIST) {
+            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+        }
+    }
 }
