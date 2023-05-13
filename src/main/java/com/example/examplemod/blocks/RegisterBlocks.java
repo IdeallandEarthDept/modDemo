019241ad91a82cee6c1b5023048ba68cb9547475
Taoism DeepLake <pymess@126.com>
2023-05-13 15:10:22 +0800

    Update RegisterBlocks.java

--- src/main/java/com/example/examplemod/blocks/RegisterBlocks.java
+++ src/main/java/com/example/examplemod/blocks/RegisterBlocks.java
@@ -1,24 +1,19 @@
 package com.example.examplemod.blocks;
 
+import com.example.examplemod.ExampleMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = ExampleMod.MODID)
 public class RegisterBlocks {
-    public static final List<Block> BLOCK_LIST = new ArrayList<>();
-    public static final Block TEST = new Block(Material.ANVIL).setRegistryName("block1");
+
 
     @SubscribeEvent
-    public static void handleBlocks(RegistryEvent.Register<Block> event)
+    public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-        event.getRegistry().register(TEST);
-        event.getRegistry().registerAll(BLOCK_LIST.toArray(new Block[0]));
+
     }
 }
