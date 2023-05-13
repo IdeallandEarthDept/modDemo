69523972b54a0e9f8ae5ef50993af9a3eae2f7d4
Taoism DeepLake <pymess@126.com>
2023-05-13 16:35:30 +0800

    Revert "block"
    This reverts commit 4980d3ce95ce6bd77afc24f38e2f4ea488eba9ca.

--- src/main/java/com/example/examplemod/RegisterUtil.java
+++ src/main/java/com/example/examplemod/RegisterUtil.java
@@ -1,10 +1,7 @@
 package com.example.examplemod;
 
-import com.example.examplemod.blocks.RegisterBlocks;
 import com.example.examplemod.items.RegisterItem;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 
 public class RegisterUtil {
     public static void initItem(Item item, String name)
@@ -12,14 +9,5 @@ public class RegisterUtil {
         item.setRegistryName(name);
         item.setUnlocalizedName(name);
         RegisterItem.ITEM_LIST.add(item);
-    }
-
-    public static void initBlock(Block block, String name)
-    {
-        block.setRegistryName(name);
-        RegisterBlocks.BLOCK_LIST.add(block);
-
-        Item item = new ItemBlock(block);
-        initItem(item, name);
     }
 }
--- src/main/java/com/example/examplemod/blocks/RegisterBlocks.java
+++ src/main/java/com/example/examplemod/blocks/RegisterBlocks.java
@@ -7,20 +7,13 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID)
 public class RegisterBlocks {
 
-    public static final List<Block> BLOCK_LIST = new ArrayList<>();
-
-    public static final Block BLOCK_1 = new BlockBase(Material.ROCK, "block1");
-    public static final Block BLOCK_2 = new BlockBase(Material.ROCK, "block2");
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
-        event.getRegistry().registerAll(BLOCK_LIST.toArray(new Block[0]));
+
     }
 }
