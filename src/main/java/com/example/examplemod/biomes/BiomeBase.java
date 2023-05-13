88c006012dd487fccc0a107948785a9670e2fa50
Taoism DeepLake <pymess@126.com>
2023-05-13 19:41:45 +0800

    Biome Rework

--- src/main/java/com/example/examplemod/biomes/BiomeBase.java
+++ src/main/java/com/example/examplemod/biomes/BiomeBase.java
@@ -3,7 +3,9 @@ package com.example.examplemod.biomes;
 import net.minecraft.world.biome.Biome;
 
 public class BiomeBase extends Biome {
-    public BiomeBase(BiomeProperties properties) {
-        super(properties);
+    public BiomeBase(String name) {
+        super(new BiomeProperties(name));
+        setRegistryName(name);
+        RegBiomes.list.add(this);
     }
 }
--- src/main/java/com/example/examplemod/biomes/RegBiomes.java
+++ src/main/java/com/example/examplemod/biomes/RegBiomes.java
@@ -9,29 +9,39 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID)
 public class RegBiomes {
 
-    @SubscribeEvent
-    public static void regBiomes(RegistryEvent.Register<Biome> event)
+    public static List<Biome> list = new ArrayList<>();
+
+    public static final Biome biome = new BiomeBase("new_biome");
+    public static final Biome biome2 = new BiomeBase("new_biome2");
+
+    static void initBiomes()
     {
-        Biome.BiomeProperties properties = new Biome.BiomeProperties("new_biome");
-        //Mess up, may skip
-        properties.setBaseHeight(-0.5f).setWaterColor(0xffffff).setHeightVariation(1.0f);
-
-        Biome biome = new BiomeBase(properties);
-        biome.setRegistryName("new_biome");
-
         //Mess up, may skip
         biome.topBlock = Blocks.LAPIS_BLOCK.getDefaultState();
         BiomeManager.addSpawnBiome(biome);
         //add to overworld, standard rate = 10
         BiomeManager.addBiome(BiomeManager.BiomeType.WARM,
                 new BiomeManager.BiomeEntry(biome, 9999));
+    }
 
-        //must register first
-        event.getRegistry().register(biome);
-
+    static void postRegBiomes()
+    {
         BiomeDictionary.addTypes(biome, BiomeDictionary.Type.COLD, BiomeDictionary.Type.DEAD);
+    }
+
+    @SubscribeEvent
+    public static void regBiomes(RegistryEvent.Register<Biome> event)
+    {
+        initBiomes();
+
+        event.getRegistry().registerAll(list.toArray(new Biome[0]));
+
+        postRegBiomes();
     }
 }
