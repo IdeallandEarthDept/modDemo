2932e8b24bac75bafca503450c4ac13c7b0f3c4b
Taoism DeepLake <pymess@126.com>
2023-06-04 10:25:51 +0800

    Update ColorRegister.java

--- src/main/java/com/example/examplemod/events/ColorRegister.java
+++ src/main/java/com/example/examplemod/events/ColorRegister.java
@@ -7,9 +7,11 @@ import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ChunkCache;
+import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeColorHelper;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.event.terraingen.BiomeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -27,29 +29,21 @@ public class ColorRegister {
     public static void registerColor(ColorHandlerEvent.Block event) {
         event.getBlockColors().registerBlockColorHandler(new IBlockColor() {
             public int colorMultiplier(IBlockState state, @Nullable IBlockAccess worldIn, @Nullable BlockPos pos, int tintIndex) {
-                if (pos != null && worldIn instanceof ChunkCache) {
-                    return getLeavesColorForPos((ChunkCache) worldIn, pos);
-                }
-                if (pos != null && worldIn instanceof World) {
-                    return getLeavesColorForPos((World) worldIn, pos);
-                }
-                else {
-                    return 0xff00ff;
-                }
+                return worldIn != null && pos != null ? BiomeColorHelper.getFoliageColorAtPos(worldIn, pos) : ColorizerFoliage.getFoliageColorBasic();
             }
         }, RegisterBlocks.BLOCK_1);
     }
 
-    public static int getLeavesColorForPos(ChunkCache world, BlockPos pos)
-    {
-        Biome biome = world.getBiome(pos);
-        return biome.getModdedBiomeFoliageColor(biome.getFoliageColorAtPos(pos));
-    }
-
-    public static int getLeavesColorForPos(World world, BlockPos pos)
-    {
-        Biome biome = world.getBiome(pos);
-        return biome.getModdedBiomeFoliageColor(biome.getFoliageColorAtPos(pos));
-    }
+//    public static int getLeavesColorForPos(ChunkCache world, BlockPos pos)
+//    {
+//        Biome biome = world.getBiome(pos);
+//        return biome.getModdedBiomeFoliageColor(biome.getFoliageColorAtPos(pos));
+//    }
+//
+//    public static int getLeavesColorForPos(World world, BlockPos pos)
+//    {
+//        Biome biome = world.getBiome(pos);
+//        return biome.getModdedBiomeFoliageColor(biome.getFoliageColorAtPos(pos));
+//    }
 
 }
